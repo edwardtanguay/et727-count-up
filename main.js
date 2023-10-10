2eeb35d862df036fb474d388a62d3339873eeca0
Edward Tanguay (windows) <edward@tanguay.info>
2023-10-10 14:59:38 +0200

    init commit

---
+++ main.js
@@ -0,0 +1,33 @@
+import './style.css';
+
+document.querySelector('#app').innerHTML = `
+<h1>Count up</h1>
+<div class="number"></div>
+`;
+
+
+const start = 0;
+const end = 200;
+const stepMilliseconds = 1;
+const numberElem = document.querySelector('.number');
+numberElem.innerText = start;
+
+
+// for (let i = 0; i <= end; i++) {
+//   setTimeout(() => {
+//     numberElem.innerText = i;
+//   }, stepMilliseconds);
+// }
+const countup = (num) => {
+  numberElem.innerText = num;
+  if (num < end) {
+    setTimeout(() => {
+      countup(num + 1);
+    }, stepMilliseconds);
+  } else {
+    console.log('blast off');
+  }
+};
+
+countup(start);
+
