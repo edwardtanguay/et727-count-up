6db4f8b5f31d2698de6f02c4401b7a8db104b771
Edward Tanguay (windows) <edward@tanguay.info>
2023-10-10 15:11:39 +0200

    both counting

--- main.js
+++ main.js
@@ -2,28 +2,40 @@ import './style.css';
 
 document.querySelector('#app').innerHTML = `
 <h1>Count up</h1>
-<div class="number number1"></div>
-<div class="number number2"></div>
+<div class="number factories"></div>
+<div class="number patents"></div>
 `;
 
-
-const start = 0;
-const end = 200;
-const stepMilliseconds = 1;
-const number1Elem = document.querySelector('.number1');
-const number2Elem = document.querySelector('.number2');
-
-const countup = (num, elem) => {
+const countup = (num, end, step, elem) => {
   elem.innerText = num;
   if (num < end) {
     setTimeout(() => {
-      countup(num + 1, elem);
-    }, stepMilliseconds);
-  } else {
-    console.log('blast off');
+      countup(num + 1, end, step, elem);
+    }, step);
   }
 };
 
-countup(start, number1Elem);
-countup(start, number2Elem);
+const statusItems = [
+  {
+    title: "factories",
+    start: 0,
+    end: 10,
+    class: 'factories',
+    step: 1
+  },
+  {
+    title: "patents",
+    start: 0,
+    end: 50,
+    class: 'patents',
+    step: 3
+  }
+];
+
+for (const statusItem of statusItems) {
+  const elem = document.querySelector('.' + statusItem.class);
+  console.log(elem);
+  countup(statusItem.start, statusItem.end, statusItem.step, elem);
+}
+
 
